cfea8344b3b306c6d569b8cced4b2f60d6d1b739
lewyuejian <lewyuejian@163.com>
2020-09-12 22:35:27 +0800

    jenkins 接口自动化测试持续集成

--- jmx/auto_stress_test.sh
+++ jmx/auto_stress_test.sh
@@ -36,10 +36,10 @@ do
     fi
     echo ${jmeter_path}
     # JMeter 静默压测
-    ${jmeter_path}/bin/jmeter -n -t ${jmx_filename} -l ${jtl_filename} -e -o ${web_report_path_name}
+    ${jmeter_path}/bin/jmeter -n -t ${jmx_filename} -l ${jtl_filename}
 
     # 生成Web压测报告
-    #${jmeter_path}/bin/jmeter -g ${jtl_filename} -e -o ${web_report_path_name}
+    ${jmeter_path}/bin/jmeter -g ${jtl_filename} -e -o ${web_report_path_name}
 
 
     rm -f ${jmx_filename} ${jtl_filename}
